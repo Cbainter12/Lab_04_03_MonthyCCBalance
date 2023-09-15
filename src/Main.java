daaf0a41669958e6e41e595a59818c08566ba9bd
Carter <carterleebainter@gmail.com>
2023-09-15 17:25:45 -0400

    Final

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
 public class Main {
     public static void main(String[] args)
     {
-        
+        int cardBalance = 5000;
+        double oneMonth = 0;
+        double twoMonth = 0;
+
+        oneMonth = cardBalance * 1.17;
+        System.out.println("Your balance after one month is: " + oneMonth);
+
+        twoMonth = oneMonth * 1.17;
+        System.out.println("Your balance after two months is: " + twoMonth);
     }
 }
